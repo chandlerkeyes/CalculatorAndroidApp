eaa73a33a83af3639ddc2a77b9985d3c027d8ce2
Chandler Keyes <chandlerkeyes@Chandlers-MacBook-Pro.local>
2018-12-29 16:31:00 -0500

    Created onClickListeners for the buttons, created landscape layout for calculator so that landscape orientation shows all buttons

--- app/src/main/java/programming/android/chandlerkeyes/calculator/MainActivity.kt
+++ app/src/main/java/programming/android/chandlerkeyes/calculator/MainActivity.kt
@@ -2,11 +2,66 @@ package programming.android.chandlerkeyes.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var result: EditText
+    private lateinit var newNumber: EditText
+    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
+
+    //operands
+    private var operand1: Double? = null
+    private var operand2: Double = 0.0
+    private var pendingOperation = "="
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        result = findViewById(R.id.result)
+        newNumber = findViewById(R.id.newNumber)
+
+        //Data input buttons
+        val button0 = findViewById<Button>(R.id.button0)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
+        val button7 = findViewById<Button>(R.id.button7)
+        val button8 = findViewById<Button>(R.id.button8)
+        val button9 = findViewById<Button>(R.id.button9)
+        val buttonDot = findViewById<Button>(R.id.buttonDot)
+
+        //Operation buttons
+
+        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
+        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
+        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
+        val buttonMultiply = findViewById<Button>(R.id.multiply)
+        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
+
+        val listener = View.OnClickListener { v ->
+            var buttonClicked = v as Button
+            newNumber.append(buttonClicked.text)
+        }
+
+        button0.setOnClickListener(listener)
+        button1.setOnClickListener(listener)
+        button2.setOnClickListener(listener)
+        button3.setOnClickListener(listener)
+        button4.setOnClickListener(listener)
+        button5.setOnClickListener(listener)
+        button6.setOnClickListener(listener)
+        button7.setOnClickListener(listener)
+        button8.setOnClickListener(listener)
+        button9.setOnClickListener(listener)
+        buttonDot.setOnClickListener(listener)
+
+
     }
 }
